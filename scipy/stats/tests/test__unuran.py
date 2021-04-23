a8a6d44874c6f7e8fe499e3d267ee9104a5cbe97
Tirth Patel <tirthasheshpatel@gmail.com>
2021-04-23 12:20:23 +0530

    TST: add tests for DAU

--- scipy/stats/tests/test__unuran.py
+++ scipy/stats/tests/test__unuran.py
@@ -1,26 +1,9 @@
 import pytest
+import warnings
 import numpy as np
 from numpy.testing import assert_allclose
 from scipy.stats._unuran import TDR, DAU
 from scipy import stats
-
-
-def test_dau():
-    from math import factorial as f
-    from math import pow
-    # check with PV.
-    rng = DAU([0.18, 0.02, 0.8], domain=(0, 2), seed=123)
-    rvs = rng.sample(size=100_000)
-    expected = (0 * 0.18 + 1 * 0.02 + 2 * 0.8)
-    assert_allclose(rvs.mean(), expected, rtol=1e-2, atol=1e-2)
-    # check with PMF.
-    pmf = lambda x, n, p : f(n)/(f(x)*f(n-x)) * pow(p, x)*pow(1-p, n-x)
-    n, p = 10, 0.2
-    with pytest.warns(UserWarning, match=r"PV. Try to compute it."):
-        rng = DAU(pmf, params=(n, p), domain=(0, n), urnfactor=2, seed=123)
-    rvs = rng.sample(size=100_000)
-    assert_allclose(rvs.mean(), n*p, rtol=1e-2, atol=1e-2)
-    assert_allclose(rvs.var(), n*p*(1-p), rtol=1e-2, atol=1e-2)
 
 
 class TestTDR:
@@ -72,7 +55,7 @@ class TestTDR:
     @pytest.mark.parametrize("domain", [(0, 0), (1, 0), (np.inf, np.inf),
                                         (-np.inf, -np.inf)])
     def test_bad_domain(self, domain):
-        with pytest.raises(ValueError, match=r"domain, left >= right"):
+        with pytest.raises(ValueError, match=r"left >= right"):
             TDR(lambda x: x, lambda x: 1, domain=domain)
 
     @pytest.mark.parametrize("domain", [(np.nan, np.nan), (np.inf, np.nan),
@@ -104,3 +87,99 @@ class TestTDR:
     def test_bad_variant(self):
         with pytest.raises(ValueError, match=r"Invalid option for the variant"):
             TDR(lambda x: x, lambda x: 1, domain=(0, 10), variant="foo")
+
+
+class TestDAU:
+    @pytest.mark.parametrize("dist, params",
+        [  # discrete distributions with finite support.
+            (stats.hypergeom, (20, 7, 12)),
+            (stats.nhypergeom, (20, 7, 12)),
+            (stats.binom, (20, 0.3))
+        ]
+    )
+    def test_sampling_with_pmf(self, dist, params):
+        domain = dist.support(*params)
+        pmf = dist._pmf
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            rng = DAU(pmf, domain=domain, params=params, seed=123)
+        rvs = rng.sample(100_000)
+        mv = rvs.mean(), rvs.var()
+        mv_expected = dist.stats(*params, moments='mv')
+        assert_allclose(mv, mv_expected, atol=1e-2)
+
+    @pytest.mark.parametrize("pv",
+        [
+            [0.18, 0.02, 0.8],
+            [1.],
+            [0., 0., 0., 0., 1.],
+            [1., 2., 3., 4., 5., 6.]
+        ]
+    )
+    def test_sampling_with_pv(self, pv):
+        pv = np.asarray(pv, dtype=np.float64)
+        rng = DAU(pv, seed=123)
+        rvs = rng.sample(100_000)
+        pv /= pv.sum()
+        variates = np.arange(0, len(pv))
+        m_expected = np.average(variates, weights=pv)
+        v_expected = np.average((variates-m_expected)**2, weights=pv)
+        mv_expected = m_expected, v_expected
+        mv = rvs.mean(), rvs.var()
+        assert_allclose(mv, mv_expected, atol=1e-2)
+
+    @pytest.mark.parametrize("pmf, msg",
+        [
+            (None, r"must not be None"),
+            (lambda x: None, r"must be real number"),
+            (lambda x: undef_name, r"name 'undef_name' is not defined"),
+            (lambda x: -x, r"probability < 0")
+        ]
+    )
+    def test_bad_pmf(self, pmf, msg):
+        with pytest.raises(Exception, match=msg):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                DAU(pmf, domain=(0, 10))
+
+    @pytest.mark.parametrize("domain", [(0, 0), (1, 0)])
+    def test_bad_domain(self, domain):
+        with pytest.raises(ValueError, match=r"left >= right"):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                DAU(lambda x: x, domain=domain)
+
+    @pytest.mark.parametrize("domain", [(np.nan, np.nan), (np.inf, np.nan),
+                                        (np.nan, -np.inf), (np.nan, 0),
+                                        (-1, np.nan), (0, float("nan"))])
+    def test_nan_domain(self, domain):
+        with pytest.raises(ValueError, match=r"must contain only non-nan values"):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                DAU(lambda x: x, domain=domain)
+
+    @pytest.mark.parametrize("domain", [(-np.inf, np.inf), (np.inf, np.inf),
+                                        (-np.inf, -np.inf), (0, np.inf),
+                                        (-np.inf, 0)])
+    def test_inf_domain(self, domain):
+        with pytest.raises(ValueError, match=r"must be finite"):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                DAU(lambda x: x, domain=domain)
+
+    @pytest.mark.parametrize("pv",
+        [
+            [0.], [], [0., 0., 0.],
+            [0., np.inf], [-np.inf, 0.],
+            [np.inf, np.inf], [-np.inf, -np.inf],
+            [np.nan], [np.nan, np.inf, -np.inf],
+            [[1., 0.], [0.5, 0.5]]
+        ]
+    )
+    def test_bad_pv(self, pv):
+        with pytest.raises(ValueError):
+            DAU(pv)
+
+    def test_bad_urnfactor(self):
+        with pytest.warns(UserWarning, match=r"relative urn size < 1."):
+            DAU([0.5, 0.5], urnfactor=-1)
